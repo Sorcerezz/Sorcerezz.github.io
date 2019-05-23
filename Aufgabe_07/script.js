6a23e0d1e9b9ceb6151e0efeb57b87511d3929da
Sorcerezz <fiona.aetherwing@gmail.com>
2019-05-23 21:10:48 +0200

    A7+

--- Aufgabe_07/script.js
+++ Aufgabe_07/script.js
@@ -2,6 +2,8 @@ let S1 = "String";
 let S2 = "Gnirts";
 let N1 = 23542;
 let N2 = 4234;
+let var1 = 1;
+var1 = 2;
 console.log("Die Website lädt noch!");
 window.onload = function () {
     console.log("Die Website hat fertig geladen!");
@@ -12,8 +14,8 @@ window.onload = function () {
     document.getElementById("b2").addEventListener("click", B2);
     document.getElementById("b3").addEventListener("click", B3p1);
     document.getElementById("b3").addEventListener("dblclick", B3p2); //Doppelklick
-    document.getElementById("b4").addEventListener("click", B4);
-    document.getElementById("b5").addEventListener("mouseover", B5); //Drüberfahren
+    document.getElementById("b4").addEventListener("mouseover", B4); //Drüberfahren
+    document.getElementById("b5").addEventListener("click", B5);
     document.getElementById("b6").addEventListener("click", B6);
     document.getElementById("b7").addEventListener("click", B7);
 };
@@ -40,7 +42,7 @@ function Knopf7() {
     let position = document.getElementById("body");
     position.appendChild(newButton);
     newButton.innerHTML = "Klick mich!";
-    newButton.id = "b6";
+    newButton.id = "b7";
 }
 function B1() {
     console.log("Knopf 1 und seine Klasse haben sich verändert");
@@ -66,8 +68,6 @@ function B4() {
 function B5() {
     console.log("Knopf 5 / Nummer N1 hat sich verändert");
     document.getElementById("b5").innerHTML = "Ich hab N1 verändert!";
-    let Number = N1;
-    let newNumber = N1 + 900;
 }
 function B6() {
     console.log("Knopf 6 hat sich verändert / ein neuer Paragraph wurde erstellt");
--- Aufgabe_07/script.ts
+++ Aufgabe_07/script.ts
@@ -3,6 +3,9 @@
     let N1: number = 23542;
     let N2: number = 4234;
 
+    let var1 : number = 1;
+    var1 = 2;
+
 console.log("Die Website lädt noch!");
 
 window.onload = function ()     //Wird Ausgeführt wenn die Seite geladen hat
@@ -15,8 +18,8 @@ window.onload = function ()     //Wird Ausgeführt wenn die Seite geladen hat
     document.getElementById("b2").addEventListener("click", B2);
     document.getElementById("b3").addEventListener("click", B3p1);
     document.getElementById("b3").addEventListener("dblclick", B3p2); //Doppelklick
-    document.getElementById("b4").addEventListener("click", B4);
-    document.getElementById("b5").addEventListener("mouseover", B5); //Drüberfahren
+    document.getElementById("b4").addEventListener("mouseover", B4); //Drüberfahren
+    document.getElementById("b5").addEventListener("click", B5);
     document.getElementById("b6").addEventListener("click", B6);
     document.getElementById("b7").addEventListener("click", B7);
 }
@@ -48,10 +51,10 @@ function Knopf7 ()  //Erstellt einen neuen Knopf der nicht in der HTML Datei vor
     let position = document.getElementById ("body");
     position.appendChild(newButton);
     newButton.innerHTML = "Klick mich!";
-    newButton.id = "b6";
+    newButton.id = "b7";
 }
 
-function B1()
+function B1() //Verändert die Klasse des Buttons
 {
     console.log("Knopf 1 und seine Klasse haben sich verändert");
     document.getElementById("b1").innerHTML="Meine Klasse hat sich verändert!";
@@ -87,10 +90,6 @@ function B5()
 {
     console.log("Knopf 5 / Nummer N1 hat sich verändert");
     document.getElementById("b5").innerHTML="Ich hab N1 verändert!";
-
-    let Number = N1;
-    let newNumber = N1 + 900; 
-
 }
 
 function B6() //Erstellt bei Klick Paragraphen
