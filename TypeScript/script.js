270ec88015f7ab8962fc62c9b90d7fe5abad8316
Sorcerezz <fiona.aetherwing@gmail.com>
2019-05-28 17:00:41 +0200

    Change

--- TypeScript/script.js
+++ TypeScript/script.js
@@ -1,10 +1,15 @@
 console.log("Die Website lädt noch!");
 window.onload = function () {
     console.log("Die Website hat fertig geladen!");
-    document.getElementById("b2").addEventListener("click", Inhalt);
+    document.getElementById("b2").addEventListener("click", Inhalt1);
+    document.getElementById("b3").addEventListener("click", Inhalt2);
 };
-function Inhalt() {
+function Inhalt1() {
     console.log("Der Text des Knopfes hat sich verändert");
     document.getElementById("b2").innerHTML = "Hier steht was neues!";
+}
+function Inhalt2() {
+    console.log("Der Text des Knopfes hat sich verändert");
+    document.getElementById("b3").innerHTML = "Hier steht auch was neues!";
 }
 //# sourceMappingURL=script.js.map
