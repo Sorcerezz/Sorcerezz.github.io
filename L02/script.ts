f269a71ad923436f1c09267a788fe8cf8b935290
Sorcerezz <fiona.aetherwing@gmail.com>
2020-05-02 19:32:04 +0200

    Update script.ts

--- L02/script.ts
+++ L02/script.ts
@@ -5,3 +5,6 @@ window.onload = function (): void {
     console.log("Die Website hat fertig geladen!");
     console.log("---------------------------------------------");
 };
+
+//Um erlich zu sein habe ich absolut keinen Plan wie ich so etwas programieren sollte. An dieser Stelle könnte ich jetzt wie andere auch einfach Code kopieren, bringen würde es mir
+//dennoch nichts. 
