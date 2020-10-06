dc586f38f12a6f7e7528d615f09d6c1c0e9bd229
Sorcerezz <fiona.aetherwing@gmail.com>
2020-10-06 12:33:31 +0200

    e

--- L02/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-console.log("Die Website lädt noch!");
-window.onload = function () {
-    console.log("Die Website hat fertig geladen!");
-    console.log("---------------------------------------------");
-};
-//Um erlich zu sein habe ich absolut keinen Plan wie ich so etwas programieren sollte. An dieser Stelle könnte ich jetzt wie andere auch einfach Code kopieren, bringen würde es mir
-//dennoch nichts. Eine kritik an dieses Fach hat hier nichts zu suchen also bleibt die Seite leer.
-//# sourceMappingURL=script.js.map
